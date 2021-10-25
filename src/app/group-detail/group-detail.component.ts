a0527b707bcf109bf44f017812db29a684d9b297
olexandrduplava <olexandrduplava@gmail.com>
2021-10-25 18:14:18 +0300

    #Commit 3.1 Create a Feature Component end part3 - "Create a Feature Component", GC - CroupComponent replaced functionality: "Now the GDC is presenting those details instead of the GC"

---
+++ src/app/group-detail/group-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Group} from "../group/group";
+
+@Component({
+  selector: 'app-group-detail',
+  templateUrl: './group-detail.component.html',
+  styleUrls: ['./group-detail.component.css']
+})
+export class GroupDetailComponent implements OnInit {
+
+  @Input() group?: Group;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
